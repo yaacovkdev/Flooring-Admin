3a138fab29697d5eb2a6deeb9f71111672cabecd
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-06-10 17:08:59 -0700

    login form

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My App</h1>
+        <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
