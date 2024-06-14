fd5fb508ec72c63953d7a84dc22cc315fa6c6f8a
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-06-13 18:56:08 -0700

    updated Hero Images

--- src/components/HeroImages/HeroImages.jsx
+++ src/components/HeroImages/HeroImages.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./HeroImages.scss";
+import axios from "axios";
 
 const HeroImages = ({ heroImagesData }) => {
   console.log(heroImagesData);
@@ -7,14 +8,15 @@ const HeroImages = ({ heroImagesData }) => {
     <div className="edit-section">
       <h2>Hero Images</h2>
       <div className="hero-images-grid">
+
         {heroImagesData.map((image, index) => (
           <div key={index} className="hero-image-row">
-            <input type="checkbox" className="checkbox" />
+            <input type="checkbox" className="checkbox"/>
             <div className="hero-image-content">
               <img width={280} src={`${process.env.REACT_APP_API_URL}/admin/image/${image.image_id}`} alt={image.file_name} className="hero-image" />
               <div className="hero-image-details">
                 <p><strong>Title:</strong> {image.file_name}</p>
-                <p><strong>Caption HTML:</strong> {image.body}</p>
+                <p><strong>Caption HTML:</strong> <input type="text" value={image.body}/></p>
                 <p><strong>Created:</strong> {image.created_at}</p>
                 <p><strong>Updated:</strong> {image.updated_at}</p>
               </div>
